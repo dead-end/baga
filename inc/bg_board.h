958990167fb7182c20108f7e2f8f51206250a96d
dead end <dead.end@inbox.ru>
2020-09-19 22:34:36 +0200

    initial version

---
+++ inc/bg_board.h
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef INC_BG_BOARD_H_
+#define INC_BG_BOARD_H_
+
+#include "bg_defs.h"
+
+/******************************************************************************
+ * The enum defines an owner of a checker which can be black or white or none
+ * if there is no checker. The owner can be used as an index for some arrays.
+ *****************************************************************************/
+
+typedef enum {
+
+	OWNER_BLACK = 0, OWNER_WHITE = 1, OWNER_NONE = -1
+
+} e_owner;
+
+#define e_owner_has_owner(o) ((o) != -1)
+
+/******************************************************************************
+ * The struct contains the number of checkers on a point and the owner of the
+ * checkers (black / white) or none if there are no checkers on the point.
+ *****************************************************************************/
+
+typedef struct {
+
+	e_owner owner;
+
+	int num;
+
+} s_bg_point;
+
+#define s_bg_point_set(p, o,n) (p)->owner = (o); (p)->num = (n)
+
+/******************************************************************************
+ * The struct contains the positions of each checker on the board..
+ *****************************************************************************/
+
+typedef struct {
+
+	//
+	// The number of checkers that are on a point for black and white.
+	//
+	s_bg_point bg_point[POINTS_NUM];
+
+	//
+	// The number of checkers that are in the bear off area for black and
+	// white. We use e_owner as an index.
+	//
+	int bear_off[2];
+
+	//
+	// The number of checkers that have to reenter for black and white. We use
+	// e_owner as an index.
+	//
+	int reenter[2];
+
+} s_bg_board;
+
+/******************************************************************************
+ * Function declarations.
+ *****************************************************************************/
+
+void bg_board_init(s_bg_board *bg_board);
+
+void bg_board_new_game(s_bg_board *bg_board);
+
+#endif /* INC_BG_BOARD_H_ */
---
+++ src/bg_board.c
@@ -0,0 +1,104 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "../inc/bg_board.h"
+
+#include "lib_logging.h"
+
+
+/******************************************************************************
+ * The definition of the index for black and white. White is playing from upper
+ * right to lower right and black is playing in reverse direction.
+ *****************************************************************************/
+
+#define idx_white(i) (i)
+
+#define idx_black(i) reverse_idx(POINTS_NUM,i)
+
+/******************************************************************************
+ * The function initializes the bg board struct. No checker are placed.
+ *****************************************************************************/
+
+void bg_board_init(s_bg_board *bg_board) {
+
+	//
+	// Initialize the points
+	//
+	for (int i = 0; i < POINTS_NUM; i++) {
+		bg_board->bg_point[i].num = 0;
+		bg_board->bg_point[i].owner = OWNER_NONE;
+	}
+
+	bg_board->bear_off[OWNER_BLACK] = 0;
+	bg_board->bear_off[OWNER_WHITE] = 0;
+
+	bg_board->reenter[OWNER_BLACK] = 0;
+	bg_board->reenter[OWNER_WHITE] = 0;
+}
+
+/******************************************************************************
+ * The function resets the bg board struct and sets the checker for the new
+ * game.
+ *****************************************************************************/
+
+void bg_board_new_game(s_bg_board *bg_board) {
+
+	bg_board_init(bg_board);
+
+	//
+	// Index 0
+	//
+	bg_board->bg_point[idx_white(0)].num = 2;
+	bg_board->bg_point[idx_white(0)].owner = OWNER_WHITE;
+
+	bg_board->bg_point[idx_black(0)].num = 2;
+	bg_board->bg_point[idx_black(0)].owner = OWNER_BLACK;
+
+	//
+	// Index 5
+	//
+	bg_board->bg_point[idx_white(5)].num = 5;
+	bg_board->bg_point[idx_white(5)].owner = OWNER_WHITE;
+
+	bg_board->bg_point[idx_black(5)].num = 5;
+	bg_board->bg_point[idx_black(5)].owner = OWNER_BLACK;
+
+	//
+	// Index 7
+	//
+	bg_board->bg_point[idx_white(7)].num = 5;
+	bg_board->bg_point[idx_white(7)].owner = OWNER_WHITE;
+
+	bg_board->bg_point[idx_black(7)].num = 5;
+	bg_board->bg_point[idx_black(7)].owner = OWNER_BLACK;
+
+	//
+	// Index 11
+	//
+	bg_board->bg_point[idx_white(11)].num = 5;
+	bg_board->bg_point[idx_white(11)].owner = OWNER_WHITE;
+
+	bg_board->bg_point[idx_black(11)].num = 5;
+	bg_board->bg_point[idx_black(11)].owner = OWNER_BLACK;
+}
