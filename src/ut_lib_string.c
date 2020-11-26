a189db4d30ba830ccfc44c81e551a2fb83abdf15
dead end <dead.end@inbox.ru>
2020-11-26 15:19:23 +0100

    added namespace and function

--- src/ut_lib_string.c
+++ src/ut_lib_string.c
@@ -29,20 +29,21 @@
 #include <stdio.h>
 
 /******************************************************************************
- * The function checks the strs_dim() function.
+ * The function checks the ls_strs_dim() function.
  *****************************************************************************/
 
-static void test_strs_dim() {
+static void test_ls_strs_dim() {
 	const char *str[] = { "1", "12", "123", "1234", "1", NULL };
 
-	const s_point dim = strs_dim(str);
+	const s_point dim = ls_strs_dim(str);
 
 	ut_check_s_point(&dim, &(s_point ) { 5, 4 }, "test_strs_dim");
 }
 
 /******************************************************************************
- * The function tests the trimming function. Remember that the trim function
- * changes the parameter, so you cannot use a literal string as an argument.
+ * The function tests the trimming function. Remember that the ls_trim()
+ * function changes the parameter, so you cannot use a literal string as an
+ * argument.
  *
  * char chr[] = "test"; // OK
  * char *chr  = "test"; // does not work
@@ -51,19 +52,59 @@ static void test_strs_dim() {
 #define STR "hallo"
 #define WHITE " \t\n "
 
-static void test_trim() {
+static void test_ls_trim() {
 
 	char chr1[] = STR WHITE;
-	const char *tr1 = trim(chr1);
+	const char *tr1 = ls_trim(chr1);
 	ut_check_char_str(tr1, STR, "trim-end");
 
 	char chr2[] = WHITE STR;
-	const char *tr2 = trim(chr2);
+	const char *tr2 = ls_trim(chr2);
 	ut_check_char_str(tr2, STR, "trim-start");
 
 	char chr3[] = WHITE STR WHITE;
-	const char *tr3 = trim(chr3);
+	const char *tr3 = ls_trim(chr3);
 	ut_check_char_str(tr3, STR, "trim-both");
+}
+
+/******************************************************************************
+ * The function checks the ls_cpy_centered() function, which copies the source
+ * string centered to the destination string.
+ *****************************************************************************/
+
+#define BUF_SIZE_1 9
+#define BUF_SIZE_2 10
+
+static void test_ls_cpy_centered() {
+	const char *from_1 = "1234";
+	const char *from_2 = "123";
+
+	char to_1[BUF_SIZE_1];
+	char to_2[BUF_SIZE_2];
+
+	//
+	// strlen(dst) - strlen(src) odd
+	//
+	ls_cpy_centered(to_1, BUF_SIZE_1, from_1);
+	ut_check_char_str(to_1, "  1234  ", "check center 1");
+
+	//
+	// strlen(dst) - strlen(src) even
+	//
+	ls_cpy_centered(to_2, BUF_SIZE_2, from_1);
+	ut_check_char_str(to_2, "  1234   ", "check center 2");
+
+	//
+	// strlen(dst) - strlen(src) even
+	//
+	ls_cpy_centered(to_1, BUF_SIZE_1, from_2);
+	ut_check_char_str(to_1, "  123   ", "check center 3");
+
+	//
+	// strlen(dst) - strlen(src) odd
+	//
+	ls_cpy_centered(to_2, BUF_SIZE_2, from_2);
+	ut_check_char_str(to_2, "   123   ", "check center 4");
 }
 
 /******************************************************************************
@@ -72,7 +113,9 @@ static void test_trim() {
 
 void ut_lib_string_exec() {
 
-	test_strs_dim();
+	test_ls_strs_dim();
 
-	test_trim();
+	test_ls_trim();
+
+	test_ls_cpy_centered();
 }
