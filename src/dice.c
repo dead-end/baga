a192f2a2ea3f6b47a7ea47751d20cf6659f81c38
dead end <dead.end@inbox.ru>
2020-12-12 22:35:19 +0100

    working state

--- src/dice.c
+++ src/dice.c
@@ -48,7 +48,6 @@
 //
 #define UP L'\u2584'
 
-// TODO: not necessary
 //
 // Lower half
 //
@@ -62,11 +61,15 @@ static const s_point _pos_dice_1 = { .row = 0, .col = 0 };
 
 static const s_point _pos_dice_2 = { .row = 0, .col = D_COLS + D_PAD };
 
+static const s_point _pos_undo = { .row = 0, .col = 2 * (D_COLS + D_PAD) };
+
+static const s_point _pos_confim = { .row = 0, .col = 3 * (D_COLS + D_PAD) };
+
 /******************************************************************************
  *
  *****************************************************************************/
 
-static const wchar_t _tmpl_wchr[6][D_ROWS][D_COLS] = {
+static const wchar_t _tmpl_dices[6][D_ROWS][D_COLS] = {
 
 {
 
@@ -146,6 +149,42 @@ static const wchar_t _tmpl_wchr[6][D_ROWS][D_COLS] = {
  *
  *****************************************************************************/
 
+static const wchar_t _tmpl_undo[D_ROWS][D_COLS] =
+
+{
+
+{ UP, UP, UP, UP, UP, UP, UP },
+
+{ FU, UP, LO, FU, LO, UP, FU },
+
+{ FU, FU, LO, UP, LO, FU, FU },
+
+{ FU, UP, FU, FU, FU, UP, FU },
+
+};
+
+/******************************************************************************
+ *
+ *****************************************************************************/
+
+static const wchar_t _tmpl_confirm[D_ROWS][D_COLS] =
+
+{
+
+{ UP, UP, UP, UP, UP, UP, UP },
+
+{ FU, FU, FU, FU, FU, LO, FU },
+
+{ FU, LO, FU, LO, UP, FU, FU },
+
+{ FU, FU, UP, FU, FU, FU, FU },
+
+};
+
+/******************************************************************************
+ *
+ *****************************************************************************/
+
 static WINDOW *_win;
 
 static s_tarr *_tmpl;
@@ -161,7 +200,13 @@ static s_tarr *_tmpl;
 #define D_STAT_INACTIVE 1
 
 // TODO: colorpairs instead of colors
-static short _colors[NUM_PLAYER][D_STATUS_NUM];
+static short _color_dices[NUM_PLAYER][D_STATUS_NUM];
+
+static short _color_ctrl_undo[D_STATUS_NUM];
+
+static short _color_ctrl_confirm[D_STATUS_NUM];
+
+static short _color_ctrl_bg[D_STATUS_NUM];
 
 /******************************************************************************
  *
@@ -182,11 +227,20 @@ void dice_init(const s_game_cfg *game_cfg, WINDOW *win) {
 	_win = win;
 	_tmpl = s_tarr_new(D_ROWS, D_COLS);
 
-	_colors[OWNER_BLACK][D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_black_active);
-	_colors[OWNER_BLACK][D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_black_inactive);
+	_color_dices[OWNER_BLACK][D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_black_active);
+	_color_dices[OWNER_BLACK][D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_black_inactive);
 
-	_colors[OWNER_WHITE][D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_white_active);
-	_colors[OWNER_WHITE][D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_white_inactive);
+	_color_dices[OWNER_WHITE][D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_white_active);
+	_color_dices[OWNER_WHITE][D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_dice_white_inactive);
+
+	_color_ctrl_confirm[D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_confirm_active);
+	_color_ctrl_confirm[D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_confirm_inactive);
+
+	_color_ctrl_undo[D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_undo_active);
+	_color_ctrl_undo[D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_undo_inactive);
+
+	_color_ctrl_bg[D_STAT_ACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_bg_active);
+	_color_ctrl_bg[D_STAT_INACTIVE] = s_color_def_hex_create(game_cfg->clr_ctrl_bg_inactive);
 }
 
 /******************************************************************************
@@ -208,7 +262,27 @@ static s_tarr* dice_get_tmpl(const int num, const short fg, const short bg) {
 		for (int col = 0; col < D_COLS; col++) {
 
 			tchar = &s_tarr_get(_tmpl, row, col);
-			tchar->chr = _tmpl_wchr[num - 1][row][col];
+			tchar->chr = _tmpl_dices[num - 1][row][col];
+			tchar->fg = fg;
+			tchar->bg = (row == 0) ? 0 : bg;
+		}
+	}
+
+	return _tmpl;
+}
+
+/******************************************************************************
+ *
+ *****************************************************************************/
+
+static s_tarr* ctl_get_tmpl(const wchar_t _tmpl_ctl[D_ROWS][D_COLS], const short fg, const short bg) {
+	s_tchar *tchar;
+
+	for (int row = 0; row < D_ROWS; row++) {
+		for (int col = 0; col < D_COLS; col++) {
+
+			tchar = &s_tarr_get(_tmpl, row, col);
+			tchar->chr = _tmpl_ctl[row][col];
 			tchar->fg = fg;
 			tchar->bg = (row == 0) ? 0 : bg;
 		}
@@ -225,8 +299,8 @@ static void get_color(const s_status *status, const bool active, short *fg, shor
 
 	const int idx = active ? D_STAT_ACTIVE : D_STAT_INACTIVE;
 
-	*fg = _colors[status->turn][idx];
-	*bg = _colors[e_owner_other(status->turn)][idx];
+	*fg = _color_dices[status->turn][idx];
+	*bg = _color_dices[e_owner_other(status->turn)][idx];
 }
 
 /******************************************************************************
@@ -238,13 +312,47 @@ void dice_print(const s_status *status) {
 
 	short fg, bg;
 
-	get_color(status, status->active == 0, &fg, &bg);
-	tmpl = dice_get_tmpl(status->dice_1, fg, bg);
+	//
+	// Dice: 0
+	//
+	get_color(status, status->dices[0].status == E_DICE_ACTIVE, &fg, &bg);
+	tmpl = dice_get_tmpl(status->dices[0].value, fg, bg);
 	s_tarr_print(_win, tmpl, _pos_dice_1);
 
-	get_color(status, status->active == 1, &fg, &bg);
-	tmpl = dice_get_tmpl(status->dice_2, fg, bg);
+	//
+	// Dice: 1
+	//
+	get_color(status, status->dices[1].status == E_DICE_ACTIVE, &fg, &bg);
+	tmpl = dice_get_tmpl(status->dices[1].value, fg, bg);
 	s_tarr_print(_win, tmpl, _pos_dice_2);
+
+	//
+	// Control: undo
+	//
+	if (status->dices[0].status == E_DICE_SET || status->dices[1].status == E_DICE_SET) {
+		bg = _color_ctrl_undo[D_STAT_ACTIVE];
+		fg = _color_ctrl_bg[D_STAT_ACTIVE];
+
+		tmpl = ctl_get_tmpl(_tmpl_undo, fg, bg);
+		s_tarr_print(_win, tmpl, _pos_undo);
+
+	} else {
+		s_tarr_print_empty(_win, (s_point ) { D_ROWS, D_COLS }, _pos_undo);
+	}
+
+	//
+	// Control: confirm
+	//
+	if (status->dices[0].status != E_DICE_ACTIVE && status->dices[1].status != E_DICE_ACTIVE) {
+		bg = _color_ctrl_confirm[D_STAT_ACTIVE];
+		fg = _color_ctrl_bg[D_STAT_ACTIVE];
+
+		tmpl = ctl_get_tmpl(_tmpl_confirm, fg, bg);
+		s_tarr_print(_win, tmpl, _pos_confim);
+
+	} else {
+		s_tarr_print_empty(_win, (s_point ) { D_ROWS, D_COLS }, _pos_confim);
+	}
 
 	wrefresh(_win);
 }
