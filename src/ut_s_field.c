3134c98381ea2e5f21768e7ab83f740876519997
dead end <dead.end@inbox.ru>
2021-01-02 22:14:35 +0100

    new tests

--- src/ut_s_field.c
+++ src/ut_s_field.c
@@ -30,30 +30,88 @@
  * The function checks the s_field_rel_idx() calls.
  *****************************************************************************/
 
-static void test_s_field_rel_idx() {
+static void test_s_field_idx_rel() {
 	s_status status;
 	int idx_rel;
 
 	status.up_2_down = OWNER_BLACK;
 
-	idx_rel = s_field_rel_idx(&status, OWNER_BLACK, 0);
+	idx_rel = s_field_idx_rel(&status, OWNER_BLACK, 0);
 	ut_check_int(idx_rel, 0, "black 0");
 
-	idx_rel = s_field_rel_idx(&status, OWNER_WHITE, 0);
-	ut_check_int(idx_rel, 23, "black 0");
+	idx_rel = s_field_idx_rel(&status, OWNER_WHITE, 0);
+	ut_check_int(idx_rel, 23, "white 0");
 
-	idx_rel = s_field_rel_idx(&status, OWNER_BLACK, 23);
+	idx_rel = s_field_idx_rel(&status, OWNER_BLACK, 23);
 	ut_check_int(idx_rel, 23, "black 23");
 
-	idx_rel = s_field_rel_idx(&status, OWNER_WHITE, 23);
-	ut_check_int(idx_rel, 0, "black 23");
+	idx_rel = s_field_idx_rel(&status, OWNER_WHITE, 23);
+	ut_check_int(idx_rel, 0, "white 23");
+}
+
+/******************************************************************************
+ * The function checks the s_field_idx_is_out() calls.
+ *****************************************************************************/
+
+static void test_s_field_idx_is_out() {
+	s_status status;
+	bool result;
+
+	status.up_2_down = OWNER_BLACK;
+
+	result = s_field_idx_is_out(&status, OWNER_BLACK, 0);
+	ut_check_bool(result, false, "black 0");
+
+	result = s_field_idx_is_out(&status, OWNER_BLACK, 23);
+	ut_check_bool(result, false, "black 23");
+
+	result = s_field_idx_is_out(&status, OWNER_BLACK, 24);
+	ut_check_bool(result, true, "black 24");
+
+	result = s_field_idx_is_out(&status, OWNER_WHITE, 23);
+	ut_check_bool(result, false, "white 23");
+
+	result = s_field_idx_is_out(&status, OWNER_WHITE, 0);
+	ut_check_bool(result, false, "white 0");
+
+	result = s_field_idx_is_out(&status, OWNER_WHITE, -1);
+	ut_check_bool(result, true, "white -1");
 
 }
+
+/******************************************************************************
+ * The function checks the test_s_field_idx_add_abs() calls.
+ *****************************************************************************/
+
+static void test_s_field_idx_add_abs() {
+	s_status status;
+	int idx;
+
+	status.up_2_down = OWNER_BLACK;
+
+	idx = s_field_idx_add_abs(&status, OWNER_BLACK, 0, 5);
+	ut_check_int(idx, 5, "black 0 - 5");
+
+	idx = s_field_idx_add_abs(&status, OWNER_WHITE, 23, 5);
+	ut_check_int(idx, 18, "white 23 - 5");
+
+	//
+	// from rel => abs
+	//
+	idx = s_field_idx_rel(&status, OWNER_WHITE, 0);
+	idx = s_field_idx_add_abs(&status, OWNER_WHITE, idx, 5);
+	ut_check_int(idx, 18, "white 0 - 5");
+}
+
 /******************************************************************************
  * The function is the a wrapper, that triggers the internal unit tests.
  *****************************************************************************/
 
 void ut_s_field_exec() {
 
-	test_s_field_rel_idx();
+	test_s_field_idx_rel();
+
+	test_s_field_idx_is_out();
+
+	test_s_field_idx_add_abs();
 }
