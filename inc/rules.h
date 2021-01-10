3e443c89a9ac26720136c675d8fdf94e9abb7cef
dead end <dead.end@inbox.ru>
2021-01-10 16:38:36 +0100

    added function

--- inc/rules.h
+++ inc/rules.h
@@ -36,4 +36,6 @@ void rules_update_phase(const s_game *game, s_status *status);
 
 int rules_min_rel_idx(const s_game *game, const s_status *status);
 
+s_field* rules_get_field_src(s_game *game, s_status *status, const s_field_id id);
+
 #endif /* INC_RULES_H_ */
--- src/rules.c
+++ src/rules.c
@@ -93,3 +93,63 @@ void rules_update_phase(const s_game *game, s_status *status) {
 
 	log_debug("Phase: %s owner: %s", e_player_phase_str(status->player_phase[status->turn]), e_owner_str(status->turn));
 }
+
+/******************************************************************************
+ * The function is called with an id of a field. The id comes from an mouse
+ * event. The function returns that field, if it is a valid source field for a
+ * move or NULL if not.
+ *****************************************************************************/
+
+s_field* rules_get_field_src(s_game *game, s_status *status, const s_field_id field_id) {
+
+	//
+	// If a checker reached the bear off area, it cannot moved again.
+	//
+	if (field_id.type == E_FIELD_BEAR_OFF) {
+		return NULL;
+	}
+
+	//
+	// Identify the source field
+	//
+	s_field *field_src = s_game_get(game, field_id);
+
+#ifdef DEBUG
+	s_field_log(field_src, "Source field");
+#endif
+
+	log_debug("Phase: %s", e_player_phase_str(status->player_phase[status->turn]));
+
+	//
+	// If there is no checker on the field, there is nothing to do.
+	//
+	if (field_src->num == 0) {
+		return NULL;
+	}
+
+	//
+	// If there is a checker we need to ensure that this is the checker of the
+	// player in turn.
+	//
+	if (field_src->owner != status->turn) {
+		return NULL;
+	}
+
+	//
+	// If the player phase is E_PHASE_BAR, we need to ensure that the source is
+	// E_FIELD_BAR. From the last check we know that the player in turn is the
+	// owner of the bar.
+	//
+	if (status->player_phase[status->turn] == E_PHASE_BAR && field_src->id.type != E_FIELD_BAR) {
+		return NULL;
+	}
+
+	//
+	// E_FIELD_BAR is only correct with E_PHASE_BAR
+	//
+	if (status->player_phase[status->turn] != E_PHASE_BAR && field_src->id.type == E_FIELD_BAR) {
+		return NULL;
+	}
+
+	return field_src;
+}
